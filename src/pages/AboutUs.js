a6bbe2940d91a402ac1856af5fa4df940756785c
kostas-python <diamantopoulos.kostas@hotmail.com>
2024-02-18 16:16:03 +0000

    so far so good

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,18 @@
+import React,{ } from 'react';
+/*import Home from './pages/Home';
+import Header from './pages/Header';
+import Footer from './pages/Footer';
+*/
+
+
+function AboutUs() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default AboutUs;
